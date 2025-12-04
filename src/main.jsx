c279421d7f197214b827bcd7b589111ebe86ee7b
Lucas Nogueira <lucasttn@MacBook-Pro-de-Lucas.local>
2025-12-04 11:33:58 -0300

    Make NewLP the default landing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import NewLP from './pages/NewLP.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NewLP />} />
+        <Route path="/lp-new" element={<NewLP />} />
+        <Route path="/lp1" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
